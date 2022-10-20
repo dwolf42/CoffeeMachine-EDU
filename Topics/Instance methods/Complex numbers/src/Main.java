064f359890ba76d67b8b46fb9724057eb3b1722b
Kabraxis <46925680+Kabraxis@users.noreply.github.com>
2022-10-20 23:59:59 +0200

    update

---
+++ Topics/Instance methods/Complex numbers/src/Main.java
@@ -0,0 +1,14 @@
+class Complex {
+
+    double real;
+    double image;
+
+    // write methods here
+    public void add(Complex num) {
+
+    }
+
+    public void subtract(Complex num) {
+
+    }
+}
