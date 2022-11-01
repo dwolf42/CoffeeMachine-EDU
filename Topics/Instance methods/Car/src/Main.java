04677d7c2179e3947de6abc679a1f5b7a52d0a7e
Kabraxis <46925680+Kabraxis@users.noreply.github.com>
2022-11-01 12:12:23 +0100

    Update course

--- Topics/Instance methods/Car/src/Main.java
+++ Topics/Instance methods/Car/src/Main.java
@@ -4,13 +4,14 @@ class Car {
     int yearModel;
     String make;
     int speed;
+    int changeSpeed = 5;
 
     public void accelerate() {
-        this.speed += 5;
+        this.speed += this.changeSpeed;
     }
 
     public void brake() {
-        this.speed -= 5;
+        this.speed -= this.changeSpeed;
         if (this.speed < 0) {
             this.speed = 0;
         }
