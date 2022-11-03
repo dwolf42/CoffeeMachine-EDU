ca4b5894a35cfbb9e2d6e22521092b0d8845e67f
Kabraxis <46925680+Kabraxis@users.noreply.github.com>
2022-11-03 20:19:18 +0100

    Update course

--- Topics/Iterating over arrays/Boxes/src/Main.java
+++ Topics/Iterating over arrays/Boxes/src/Main.java
@@ -2,6 +2,22 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        // write your code here
+        Scanner scanner = new Scanner(System.in);
+        int[] box1 = {scanner.nextInt(), scanner.nextInt(), scanner.nextInt()};
+        int[] box2 = {scanner.nextInt(), scanner.nextInt(), scanner.nextInt()};
+        Arrays.sort(box1);
+        Arrays.sort(box2);
+        int a = 0;
+        int b = 0;
+
+        for (int i = 0; i < box1.length; i++) {
+            if (box1[i] < box2[i]) {
+                b++;
+            } else if (box1[i] > box2[i]){
+                a++;
+            }
+        }
+
+        System.out.println(a == 3 ? "Box 1 > Box 2" : b == 3 ? "Box 1 < Box 2" : "Incompatible");
     }
 }
--- Topics/Iterating over arrays/Cyclically shifting elements/src/Main.java
+++ Topics/Iterating over arrays/Cyclically shifting elements/src/Main.java
@@ -4,5 +4,20 @@ class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         // put your code here
+        int arrayLength = scanner.nextInt();
+        int[] inputArray = new int[arrayLength];
+        for (int i = 0; i < inputArray.length; i++) {
+            inputArray[i] = scanner.nextInt();
+        }
+        int temp = inputArray[inputArray.length - 1];
+        int[] outputArray = new int[inputArray.length];
+        for (int i = 1; i < outputArray.length; i++) {
+            outputArray[i] = inputArray[i - 1];
+        }
+        outputArray[0] = temp;
+
+        for (int indexes : outputArray) {
+            System.out.print(indexes + " ");
+        }
     }
 }
