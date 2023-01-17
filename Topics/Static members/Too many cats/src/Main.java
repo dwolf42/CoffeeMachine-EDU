61d689b619597b55779a1b73402bf83dfa9de298
Kabraxis <46925680+Kabraxis@users.noreply.github.com>
2023-01-17 16:02:36 +0100

    update

---
+++ Topics/Static members/Too many cats/src/Main.java
@@ -0,0 +1,21 @@
+
+class Cat {
+    String name;
+    int age;
+    static int counter = 0;
+    final int maxCats = 5;
+
+    public Cat(String name, int age) {
+        counter++;
+        if (Cat.counter > maxCats) {
+            System.out.println("You have too many cats");
+        }
+        this.name = name;
+        this.age = age;
+
+    }
+
+    public static int getNumberOfCats() {
+        return counter;
+    }
+}
