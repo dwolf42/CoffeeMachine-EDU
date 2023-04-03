d773c9ea0f8d1d2cf05a6d1d8e9a2b61c141522a
Kabraxis <46925680+Kabraxis@users.noreply.github.com>
2023-04-03 14:40:47 +0200

    update

---
+++ Topics/Multidimensional array/The third row/src/Main.java
@@ -0,0 +1,11 @@
+class ArrayOperations {
+    public static void printTheThirdRow(int[][] twoDimArray) {
+        final int rowToPrintIndex = 2;
+        for (int i = rowToPrintIndex; i == rowToPrintIndex; i++) {
+            for (int j = 0; j < twoDimArray[1].length; j++) {
+                System.out.print(twoDimArray[i][j] + " ");
+            }
+        }
+    }
+
+}
